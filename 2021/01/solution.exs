e22f7e5eee9a608830b5d5d20c0258998a924a53
Brian Berlin <bri@nberl.in>
2021-12-02 09:07:01 -0600

    pipes all the way down
    Update solution.exs

--- 2021/01/solution.exs
+++ 2021/01/solution.exs
@@ -1,11 +1,3 @@
-count_greater_measurements = fn depth, {previous_depth, num} ->
-  if depth > previous_depth do
-    {depth, num + 1}
-  else
-    {depth, num}
-  end
-end
-
 File.cwd!()
 |> Path.join("2021/01/input.txt")
 |> File.read!()
@@ -13,7 +5,6 @@ File.cwd!()
 |> Enum.map(&String.to_integer/1)
 |> Enum.chunk_every(3, 1, :discard)
 |> Enum.map(&Enum.sum/1)
-|> Enum.reduce({0, 0}, count_greater_measurements)
-|> elem(1)
-|> Kernel.-(1)
+|> Enum.chunk_every(2, 1, :discard)
+|> Enum.reduce(0, fn [a, b], num -> if(b > a, do: num + 1, else: num) end)
 |> IO.inspect()
